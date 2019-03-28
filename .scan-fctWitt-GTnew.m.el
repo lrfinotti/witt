92fa448478384ca4f8b05f372d72a0480e8e90a6
Luis R Finotti <luis.finotti@gmail.com>
2019-03-28 09:18:39 -0400

    Deleted old files.

--- .scan-fctWitt-GTnew.m.el
+++
@@ -1,4 +0,0 @@
-;;; This file was generated automatically.
-
-(magma-load-or-rescan (f-expand "gt.m" magma-working-directory))
-(setq magma-completion-table (-union magma-completion-table '("lift" "my_int" )))
