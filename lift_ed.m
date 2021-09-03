bb5c551697ff18b0a3f2b363390efc0df7fb805a
Luis R Finotti <luis.finotti@gmail.com>
2021-09-03 07:50:57 -0400

    Added attempted to single var.  (Does not work!)

--- lift_ed.m
+++ lift_ed.m
@@ -173,7 +173,7 @@ function lift_ed(a0,b0,n : pols:=[], verb:=true, cstr:=[], check:=true, minimal:
         if i gt 1 then
 
             if verb then
-                print "Coputing higher degree terms.";
+                print "Computing higher degree terms.";
             end if;
 
             // to invert
@@ -389,13 +389,13 @@ function lift_ed_1(a0,n : pols:=[], verb:=true, cstr:=[], check:=true, minimal:=
         if i gt 1 then
 
             if verb then
-                print "Coputing higher degree terms.";
+                print "Computing higher degree terms.";
             end if;
 
             // to invert
             Fi := FieldOfFractions(Pi);
 
-            vx:=[ Fi!Evaluate(x,[Pi.1,Pi.2]) : x in resx ] cat [Fi.(M+4)];
+            vx:=[ Fi!Evaluate(x,[Pi.1,Pi.2]) : x in resx ] cat [Fi.(M+4) ];
 
             // xi
             vinvi:=WittInv(vx : pols:=pols)[i+1];
@@ -419,16 +419,21 @@ function lift_ed_1(a0,n : pols:=[], verb:=true, cstr:=[], check:=true, minimal:=
         vy:=[ Evaluate(y,[Pi.1,Pi.2]) : y in resy ] cat [yi];
         vone:=[Pi!1] cat [ Pi!0 : j in [1..i]];
 
-        vvars:=vx cat vy;
+        vx2:=WittPower(vx,2 : pols:=pols);
+        vy2:=[ Evaluate(x,[Pi.2,Pi.1] cat [Pi.j : j in [3..(M+4)]]) : x in vx2 ];
 
+        // vvars:=vx cat vy;
+        vvars:=vx2 cat vy2;
 
         // Compute the Greenberg Transform
         if verb then
             print "Computing the GT.";
         end if;
 
-        LeftGT:=GT( [[* vone,2,0 *], [* vone,0,2 *]] : pols:=pols, vvars:=vvars);
-        RightGT:=GT( [[* va,2,2 *], [* vone,0,0 *]] : pols:=pols, vvars:=vvars);
+        // LeftGT:=GT( [[* vone,2,0 *], [* vone,0,2 *]] : pols:=pols, vvars:=vvars);
+        // RightGT:=GT( [[* va,2,2 *], [* vone,0,0 *]] : pols:=pols, vvars:=vvars);
+        LeftGT:=GT( [[* vone,1,0 *], [* vone,0,1 *]] : pols:=pols, vvars:=vvars);
+        RightGT:=GT( [[* va,1,1 *], [* vone,0,0 *]] : pols:=pols, vvars:=vvars);
 
         difference:=RightGT[i+1] - LeftGT[i+1];
 
@@ -482,6 +487,187 @@ function lift_ed_1(a0,n : pols:=[], verb:=true, cstr:=[], check:=true, minimal:=
         Append(~resa,vsol[1]);
         Append(~resx,Evaluate(xi,evalvec));
         Append(~resy,Evaluate(yi,evalvec));
+
+    end for;
+
+    return resa, resx, resy;
+
+end function;
+
+
+
+function lift_ed_2(a0,n : pols:=[], verb:=true, cstr:=[], check:=true, minimal:=false)
+
+    F:=Parent(a0);
+    b0:=F!1;
+    p:=Characteristic(F);
+
+    if #pols eq 0 then
+            pols:=etapols(p,n);
+    end if;
+
+    Pres<x0,y0>:=PolynomialRing(F,2);
+
+    /* CurveEq := b0^2*x0^2 + y0^2 - 1 - a0*x0^2*y0^2; */
+
+    /* // introduce variables for the quotient */
+    /* C<xx0, yy0>:=quo<Pres | CurveEq>; */
+
+    /* // let's get the conversion maps */
+    /* PtoC := hom< Pres -> C | [xx0, yy0]>; */
+    /* CtoP := hom< C -> Pres | [x0, y0]>; */
+
+    resa:=[a0];
+    resx:=[x0];
+    resy:=[y0];
+
+
+    // FIXME: do we need two?
+    xf:=(x0^2-1)*(a0*x0^2-1); // Important Equation used in derivative of x_n
+
+    ff:=xf^((p-1) div 2);
+
+    HI:=F!(Coefficient(ff, 1, p-1)); //Hasse Invaraint
+
+    if HI eq 0 then
+            return "This Curve is not Ordinary";
+    end if;
+
+
+    //main loop
+    for i in [1..n] do
+        if verb then
+            print "Working on i equals: ", i;
+        end if;
+
+        // necessary variables
+        // we only need the coefficients below the degree of the derivative
+        // we do not need "+1" since constant terms are zero
+        M:=2*p^(i-1)-1;
+
+        // print "The value of M is: ", M;
+
+        Pi:=PolynomialRing(F,1 + 1 + M + 1);
+        // x_0, a_n, epsilon_i' (recall epsilon_0=delta_0=0)
+        // plus one additional variable to help with inverses in the computation
+        // of higher degree terms
+
+        // a temporary power of f to help compute x_i's and y_i's:
+        if i eq 1 then
+                tmppf:=ff;
+        else
+                tmppf:=tmppf^p*ff;
+        end if;
+
+        xi:= HI^(-(p^i-1) div (p-1))*tmppf - x0^(p^i-1);
+        if i gt 1 then
+                xi-:=&+[ resx[j+1]^(p^(i-j)-1)*Derivative(resx[j+1],1) :
+                        j in [1..(i-1)] ];
+        end if;
+
+        xi:=my_x_int(xi);
+        xi:=Evaluate(xi,[Pi.1,0]);
+
+        // NOT YET: will make epsilon_(p^(n-1))=0
+        // we have epsilon_0 = 0
+        // xi+:=&+[ Pi.(5+j)*(Pi.1)^(p*j) : j in [1..M] | j ne p^(i-1) ];
+        xi+:=&+[ Pi.(2+j)*(Pi.1)^(p*j) : j in [1..M] ];
+
+
+
+
+        // find higher degree terms
+        if i gt 1 then
+
+            if verb then
+                print "Computing higher degree terms.";
+            end if;
+
+            // to invert
+            Fi := FieldOfFractions(Pi);
+
+            vx:=[ Fi!Evaluate(x,[Pi.1,0]) : x in resx ] cat [Fi.(M+3)];
+
+            // xi
+            vinvi:=WittInv(vx : pols:=pols)[i+1];
+            num:=Pi!Numerator(vinvi);
+            den:=Pi!Denominator(vinvi);
+
+            rem_terms:=Coefficient(num,Pi.(M+3),0); // terms without xi
+
+            degn:=Degree(Coefficient(num,Pi.(M+3),1),1);
+            degd := Degree(den,Pi.1);
+
+            for k in [((degd-degn) div p)..((i+1)*p^(i-1)-i*p^(i-2))] do
+                xi +:= Coefficient(rem_terms,1,k*p+degn)*(Pi.1)^(k*p);
+            end for;
+
+        end if;
+
+        va:=[ Pi!x : x in resa ] cat [Pi.2];
+        vx:=[ Evaluate(x,[Pi.1,Pi.2]) : x in resx ] cat [xi];
+        vx2:=WittPower(vx, 2 : pols:=pols);
+        vzero:=[ Pi!0 : j in [1..(i+1)]];
+        vone:=[Pi!1] cat [ Pi!0 : j in [1..i]];
+        vtwo:=[ Pi!x : x in IntToWitt(2,p,i) ];
+
+        vvars:=vx2 cat vzero;
+
+
+        // Compute the Greenberg Transform
+        if verb then
+            print "Computing the GT.";
+        end if;
+
+        // LeftGT:=GT( [[* vtwo,2,0 *]] : pols:=pols, vvars:=vvars );
+        LeftGT:=WittProd(vtwo,vx2 : pols:=pols);
+        RightGT:=GT( [[* va,2,0 *], [* vone,0,0 *]] : pols:=pols, vvars:=vvars );
+
+        difference:=RightGT[i+1] - LeftGT[i+1];
+
+        // We need to add the extra conditions from tau(0,1)=(0,1)
+        maxd := Degree(difference,1);
+
+        // this adds that xi(1) = yi(1) = 0
+        difference +:= Evaluate(xi, Pi.1, 1)*Pi.1^(maxd + 1);
+
+        // No reduction?
+
+       // collect the coefficients, which then have to be zero
+        eqts:=[];
+        for coefx in Coefficients(difference, Pi.1) do
+            if coefx ne 0 then
+                Append(~eqts, coefx);
+            end if;
+        end for;
+
+        neqts:=#eqts;
+
+        // print eqts;
+
+        Mat:=Matrix(F,1 + M,neqts,
+                [[Coefficient(eqts[j],k+1,1) : j in [1..neqts]]
+                        : k in [1..(1+M)]]);
+
+        vec:=Vector(F,
+                 [ -Evaluate(eqts[j], [ 0 : k in [1..(2+M+1)] ]) : j in [1..neqts] ]);
+
+        if verb then
+            print "Solving the system";
+        end if;
+
+        vsol:=Solution(Mat,vec);
+
+        // to convert solutions to Pres/F
+        evalvec:=[x0] cat [ vsol[j] : j in [1..1+M]] cat [0];
+
+       // print #evalvec;
+
+        xxi:=Evaluate(xi,evalvec);
+
+        Append(~resa,vsol[1]);
+        Append(~resx,xxi);
+        Append(~resy,Evaluate(xxi,[y0,0]));
 
     end for;
 
